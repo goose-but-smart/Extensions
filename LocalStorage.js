331e75d88a143bdfe50f1b25c7e1f5589cdf0236
goose-but-smart <151053282+goose-but-smart@users.noreply.github.com>
2023-11-16 19:40:31 +0300

    Create LocalStorage.js

---
+++ LocalStorage.js
@@ -0,0 +1,178 @@
+/*
+   This extension was made with TurboBuilder!
+   https://turbobuilder-steel.vercel.app/
+*/
+(function(Scratch) {
+    const variables = {};
+    const blocks = [];
+    const menus = [];
+
+
+    function doSound(ab, cd, runtime) {
+        const audioEngine = runtime.audioEngine;
+
+        const fetchAsArrayBufferWithTimeout = (url) =>
+            new Promise((resolve, reject) => {
+                const xhr = new XMLHttpRequest();
+                let timeout = setTimeout(() => {
+                    xhr.abort();
+                    reject(new Error("Timed out"));
+                }, 5000);
+                xhr.onload = () => {
+                    clearTimeout(timeout);
+                    if (xhr.status === 200) {
+                        resolve(xhr.response);
+                    } else {
+                        reject(new Error(`HTTP error ${xhr.status} while fetching ${url}`));
+                    }
+                };
+                xhr.onerror = () => {
+                    clearTimeout(timeout);
+                    reject(new Error(`Failed to request ${url}`));
+                };
+                xhr.responseType = "arraybuffer";
+                xhr.open("GET", url);
+                xhr.send();
+            });
+
+        const soundPlayerCache = new Map();
+
+        const decodeSoundPlayer = async (url) => {
+            const cached = soundPlayerCache.get(url);
+            if (cached) {
+                if (cached.sound) {
+                    return cached.sound;
+                }
+                throw cached.error;
+            }
+
+            try {
+                const arrayBuffer = await fetchAsArrayBufferWithTimeout(url);
+                const soundPlayer = await audioEngine.decodeSoundPlayer({
+                    data: {
+                        buffer: arrayBuffer,
+                    },
+                });
+                soundPlayerCache.set(url, {
+                    sound: soundPlayer,
+                    error: null,
+                });
+                return soundPlayer;
+            } catch (e) {
+                soundPlayerCache.set(url, {
+                    sound: null,
+                    error: e,
+                });
+                throw e;
+            }
+        };
+
+        const playWithAudioEngine = async (url, target) => {
+            const soundBank = target.sprite.soundBank;
+
+            let soundPlayer;
+            try {
+                const originalSoundPlayer = await decodeSoundPlayer(url);
+                soundPlayer = originalSoundPlayer.take();
+            } catch (e) {
+                console.warn(
+                    "Could not fetch audio; falling back to primitive approach",
+                    e
+                );
+                return false;
+            }
+
+            soundBank.addSoundPlayer(soundPlayer);
+            await soundBank.playSound(target, soundPlayer.id);
+
+            delete soundBank.soundPlayers[soundPlayer.id];
+            soundBank.playerTargets.delete(soundPlayer.id);
+            soundBank.soundEffects.delete(soundPlayer.id);
+
+            return true;
+        };
+
+        const playWithAudioElement = (url, target) =>
+            new Promise((resolve, reject) => {
+                const mediaElement = new Audio(url);
+
+                mediaElement.volume = target.volume / 100;
+
+                mediaElement.onended = () => {
+                    resolve();
+                };
+                mediaElement
+                    .play()
+                    .then(() => {
+                        // Wait for onended
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            });
+
+        const playSound = async (url, target) => {
+            try {
+                if (!(await Scratch.canFetch(url))) {
+                    throw new Error(`Permission to fetch ${url} denied`);
+                }
+
+                const success = await playWithAudioEngine(url, target);
+                if (!success) {
+                    return await playWithAudioElement(url, target);
+                }
+            } catch (e) {
+                console.warn(`All attempts to play ${url} failed`, e);
+            }
+        };
+
+        playSound(ab, cd)
+    }
+    class Extension {
+        getInfo() {
+            return {
+                "id": "localstoragebysv4",
+                "name": "Local Storage",
+                "color1": "#000000",
+                "color2": "#ffffff",
+                "color3": "#ffffff",
+                "blocks": blocks
+            }
+        }
+    }
+    blocks.push({
+        opcode: `setlocal`,
+        blockType: Scratch.BlockType.COMMAND,
+        text: `Set [1] to [2]`,
+        arguments: {
+            "1": {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Local',
+            },
+            "2": {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'value',
+            },
+        }
+    });
+    Extension.prototype[`setlocal`] = (args, util) => {
+        localStorage.setItem(args["1"], args["2"])
+    };
+
+    blocks.push({
+        opcode: `getlocal`,
+        blockType: Scratch.BlockType.REPORTER,
+        text: `Get [3]`,
+        arguments: {
+            "3": {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Local',
+            },
+        }
+    });
+    Extension.prototype[`getlocal`] = (args, util) => {
+        return localStorage.getItem(args["3"])
+    };
+
+    Scratch.extensions.register(new Extension());
+})(Scratch);
